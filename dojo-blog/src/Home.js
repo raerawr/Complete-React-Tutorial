10fe6b75237f4874e0ebcef06ccb9ae614734ae9
raerawr <121990754+raerawr@users.noreply.github.com>
2026-03-20 01:11:21 -0500

    lesson 18

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,20 +3,25 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     //npx json-server --watch data/db.json --port 8000
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-            })
+        setTimeout(() => {
+                fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                });
+            }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
