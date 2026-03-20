7dd058c03d89a75eee68457410aa9938bd806ce9
raerawr <121990754+raerawr@users.noreply.github.com>
2026-03-20 01:18:01 -0500

    lesson 19

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,23 +4,33 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     //npx json-server --watch data/db.json --port 8000
     useEffect(() => {
         setTimeout(() => {
-                fetch('http://localhost:8000/blogs')
+            fetch('http://localhost:8000/blogs')
                 .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fetch the data for that resource');
+                    }
                     return res.json();
                 })
                 .then(data => {
                     setBlogs(data);
                     setIsPending(false);
-                });
-            }, 1000);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{ error }</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
