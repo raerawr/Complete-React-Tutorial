3092af69ec4c191cc658ff1f71bbb25456b567df
raerawr <121990754+raerawr@users.noreply.github.com>
2026-03-20 01:06:44 -0500

    lesson 17

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,24 +2,22 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id != id);
-        setBlogs(newBlogs);
-    }
-
+    //npx json-server --watch data/db.json --port 8000
     useEffect(() => {
-        console.log('use effect ran');
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
     }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
 }
