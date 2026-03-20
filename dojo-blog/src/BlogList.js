43d5d27005d262fe7ae61099093cfe8da1234c08
raerawr <121990754+raerawr@users.noreply.github.com>
2026-03-20 00:31:03 -0500

    lesson 13

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,6 +1,5 @@
-const BlogList = ({ blogs, title }) => {
-    //const blogs = props.blogs;
-    //const title = props.title;
+const BlogList = ({ blogs, title, handleDelete }) => {
+    
 
     return ( 
         <div className="blog-list">
@@ -9,6 +8,7 @@ const BlogList = ({ blogs, title }) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title } </h2>
                     <p>Written by { blog.author }</p>
+                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
