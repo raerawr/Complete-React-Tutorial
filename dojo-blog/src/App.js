d79a68f3e648e1dfe6fa5a0b86e92c448e3b2a53
raerawr <121990754+raerawr@users.noreply.github.com>
2026-03-19 23:27:21 -0500

    lesson 5

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,26 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  //const person = { name: 'yoshi', age: 30 };
-  // cannot print out booleans and/or objects
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content"></div>
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/*<p>{ person }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={link}>Google Site</a>
+        <Home />
     </div>
   );
 }
