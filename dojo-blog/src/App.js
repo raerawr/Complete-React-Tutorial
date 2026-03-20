9e35bb4c985c71a6e6cf97fc57cda347bc58a417
raerawr <121990754+raerawr@users.noreply.github.com>
2026-03-20 02:13:54 -0500

    lesson 21

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,13 +1,20 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content"></div>
-        <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content"></div>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
